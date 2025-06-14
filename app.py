70ea4192e14dfb9bf89b7b9d8f9cd7f68d22072f
Michael Lopes <107208892+michaufsc@users.noreply.github.com>
2025-06-14 20:46:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -302,7 +302,8 @@ def main():
     st.header("Museu do Lixo - COMCAP Florianópolis ♻️")
     st.subheader("Aplicativo para educadores: Química dos resíduos")
     st.markdown("*Desenvolvido durante a disciplina de Prática de Ensino em Espaços de Divulgação Científica (Ext 18h-a)*")
-   # Divisória superior
+    
+    # Divisória superior
     st.markdown("---")
     st.markdown("---")
 
@@ -328,19 +329,18 @@ def main():
         mostrar_compostagem()
     with tab6:
         mostrar_quimica()
-   with tab7:
-    st.header("Sobre o Projeto")
-    st.markdown("""
+    with tab7:
+        st.header("Sobre o Projeto")
+        st.markdown("""
 **Glossário Interativo de Resíduos e Polímeros**  
 - Desenvolvido para educação ambiental  
 - Dados técnicos baseados em normas ABNT  
 - Integrado com atividades pedagógicas  
 
 **Autor:** nome alunos e prof  
-"**Disciplina:** Prática de Ensino em Espaços de Divulgação Científica (Ext 18h\-a)"
+**Disciplina:** Prática de Ensino em Espaços de Divulgação Científica (Ext 18h-a)  
 **Instituição:** Universidade Federal de Santa Catarina (UFSC)
 """)
-
 
 if __name__ == "__main__":
     if not os.path.exists(IMAGES_DIR):
