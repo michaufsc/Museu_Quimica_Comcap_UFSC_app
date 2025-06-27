21f4681687e80748a0ae4c92463a9faf8351d607
Michael Lopes <107208892+michaufsc@users.noreply.github.com>
2025-06-27 10:07:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -983,7 +983,6 @@ def mostrar_cooperativas():
         st.caption("📍 Clique nos marcadores para ver detalhes")
         
 #função plástico nos oceanos
-def mostrar_plastico_oceanos():
    def mostrar_plastico_oceanos():
     st.header("🔍 Microplásticos no Litoral Catarinense - O Que Dizem as Pesquisas?")
     
